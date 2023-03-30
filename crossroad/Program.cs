07a9793ad64f1a2c50e00d3413e93e7fc2ee48ec
EdvinGitHub <Edvin.arvesjo@elev.ga.ntig.se>
2023-03-30 16:29:50 +0200

    30/32023 part 2

--- crossroad/Program.cs
+++ crossroad/Program.cs
@@ -2,8 +2,8 @@
 using System.Numerics;
 
 
-int screenwidht= 1550;
-int screenhight= 800;
+int screenwidht = 1550;
+int screenhight = 800;
 
 
 Raylib.InitWindow(screenwidht, screenhight, "fun games");
@@ -14,61 +14,102 @@ Raylib.SetTargetFPS(60);
 // Thing test = new Thing();
 
 //  movementSprites
-Rectangle player = new Rectangle(1500/2, 750, 50, 50);
+Rectangle player = new Rectangle(1500 / 2, 750, 50, 50);
+Rectangle test = new Rectangle(2000 / 2, 500, 50, 50);
 
-List<Rectangle> walls = new List<Rectangle>();
-walls.Add(new Rectangle(1500,800, 100, -1500));
-walls.Add(new Rectangle(1500,0, 100, 1500));
-walls.Add(new Rectangle(0,800, 100, 1500));
-walls.Add(new Rectangle(0,0, 100, 1500));
+// Random genarator =
 
 while (!Raylib.WindowShouldClose())
 {
+    player.y++;
+
+
     Raylib.BeginDrawing();
 
     Raylib.ClearBackground(Color.WHITE);
-    
-    Raylib.DrawRectangleRec(player,Color.YELLOW);
 
-    if (player.x >= screenwidht || player.x <= -screenwidht || player.y >= screenhight || player.y <= -screenhight ){
+    Raylib.DrawRectangleRec(player, Color.YELLOW);
+    Raylib.DrawRectangleRec(test, Color.BROWN);
+
+
+
+
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_A))
+    {
+        player.x -= 75;
+    }
+    if (player.x < 0)
+    {
+        player.x = 0;
+    }
+
+
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_D))
+    {
+        player.x += 75;
+    }
+    if (player.x >= 1550)
+    {
+        player.x = 1550;
+    }
+    
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_W))
+    {
+        player.y -= 75;
+    }
+    if (player.y <= 0)
+    {
+        player.y = 0;
+    }
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_S))
+    {
+        player.y += 75;
+    }
+    if (player.y >= 750)
+    {
+            player.y -= 75;
+    }
         
-    if(Raylib.IsKeyPressed(KeyboardKey.KEY_W)){
-        player.y += 75;
-    }
-    if(Raylib.IsKeyPressed(KeyboardKey.KEY_S)){
-        player.y -= 75;
-    }
-    if(Raylib.IsKeyPressed(KeyboardKey.KEY_A)){
-        player.x += 75;
-    }
-    if(Raylib.IsKeyPressed(KeyboardKey.KEY_D)){
-        player.x -= 75;
-    }
 
-    }
-    else {
-    if(Raylib.IsKeyPressed(KeyboardKey.KEY_W)){
-        player.y -= 75;
-    }
-    if(Raylib.IsKeyPressed(KeyboardKey.KEY_S)){
-        player.y += 75;
-    }
-    if(Raylib.IsKeyPressed(KeyboardKey.KEY_A)){
-        player.x -= 75;
-    }
-    if(Raylib.IsKeyPressed(KeyboardKey.KEY_D)){
-        player.x += 75;
-    }
-
-    }
-
-    //        foreach (Rectangle wall in walls)
+    // if (Raylib.IsKeyPressed(KeyboardKey.KEY_W))
     // {
-    //     Raylib.DrawRectangleRec(wall, Color.BLACK);
+    //     player.y -= 75;
     // }
-    
-        Raylib.EndDrawing();
-    }
+    // if (Raylib.IsKeyPressed(KeyboardKey.KEY_S))
+    // {
+    //     player.y += 75;
+    // }
+    // if (Raylib.IsKeyPressed(KeyboardKey.KEY_A))
+    // {
+    //     player.x -= 75;
+    // }
+    // if (Raylib.IsKeyPressed(KeyboardKey.KEY_D))
+    // {
+    //     player.x += 75;
+    // }
+    // if (player.x >= screenwidht || player.x <= -screenwidht || player.y >= screenhight || player.y <= -screenhight ){
+
+    // if(Raylib.IsKeyPressed(KeyboardKey.KEY_W)){
+    //     player.y += 75;
+    // }
+    // if(Raylib.IsKeyPressed(KeyboardKey.KEY_S)){
+    //     player.y -= 75;
+    // }
+    // if(Raylib.IsKeyPressed(KeyboardKey.KEY_A)){
+    //     player.x += 75;
+    // }
+    // if(Raylib.IsKeyPressed(KeyboardKey.KEY_D)){
+    //     player.x -= 75;
+    // }
+
+    // }
 
 
-  
+
+
+
+
+    Raylib.EndDrawing();
+}
+
+
