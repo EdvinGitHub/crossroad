bf40c6f57303ad4945065052bb56e3f36b39ce8f
EdvinGitHub <Edvin.arvesjo@elev.ga.ntig.se>
2023-04-20 13:42:13 +0200

    lekk

--- crossroad/Program.cs
+++ crossroad/Program.cs
@@ -32,7 +32,8 @@ float enamyX2 = 0;
 // int enamyhight = 0;
 float enamyY = 0;
 float enamyY2 = 0;
-float enamyspeed = 0;
+float enamyspeed1 = 0;
+float enamyspeed2 = 0;
 // genarator.Next(screenhight)
 List<Rectangle> enamy = new List<Rectangle>();
 
@@ -48,14 +49,19 @@ int timer = 0;
 while (!Raylib.WindowShouldClose())
 {
     player.y++;
-
+    enamys.y ++;
+    enamys2.y ++;
+    enamys.x += enamyspeed1;
+    enamys2.x += enamyspeed2;
     timer++;
-    
-    enamys.y += 4;
-    enamys2.y += 4;
-
-    enamys.x += enamyspeed;
-    enamys2.x += enamyspeed;
+    if (enamyspeed1 >= 0)
+    {
+    enamys.y += 3;
+    }
+    if(enamyspeed2 >= 0)
+    {
+    enamys2.y += 3;
+    }
     while(i2 == 0)
     {
             enamy.Add(enamys = new Rectangle(enamyX,enamyY2,100,50));
@@ -68,62 +74,65 @@ while (!Raylib.WindowShouldClose())
         timer = 0;
         // gör nåt
         i1 = genarator.Next(2);
+        enamyX2 = enamys.x;
+        enamyX = enamys2.x;
+        enamyY2 = enamys.y; 
+        enamyY = enamys2.x; 
+        if(enamys2.x>=1500)
+        {
+        enamys2.y = -100;
+        enamyX = -100;
+        }
+        if(enamys.x>=1500)
+        {
+        enamys.y = -100;
+        enamyX2 = -100;
+        
+        }
+        if(enamys.y >= 800)
+        {
+        enamys.y = -100;
+        enamyX = -100;
+        }
+        if(enamys2.y >= 800)
+        {
+        enamys2.y = -100;
+        enamyX2 = -100;
+        }
         if(i1 == 0)
         {   
-            enamyY2 = enamys.y; 
-            enamyY = enamys2.x; 
 
-            if(enamys2.x>=1500)
-            {
-            enamys2.y = 0;
-            enamyX2 = -100;
-            }
-            if(enamys.x>=1500)
-            {
-            enamys.y = 0;
-            enamyX = -100;
-            }
-            if(enamys.y >= 800)
-            {
-            enamys.y = 0;
-            enamyX = -100;
-            }
-            if(enamys2.y >= 800)
-            {
-            enamys2.y = 0;
-            enamyX2 = -100;
-            }
-            if(enamys.x <= -100) {
+            if(enamyX <= -100) {
             enamys.x = genarator.Next(1400);
             enamyX = enamys.x;
+            enamys.y = 0;
+            enamyspeed1 = 0;
             }
-            else if(enamys2.x <= -100)
+            if(enamyX2 <= -100)
             {
             enamys2.x = genarator.Next(1400);
             enamyX2 = enamys2.x;
-            // enamyX = genarator.Next(1500);
-            // enamy.Add(enamys = new Rectangle(enamyX,enamyY2,100,50));
-                enamyspeed = 0;
+            enamys2.y = 0;
+            enamyspeed2 = 0;
             }
         }
         if(i1==1)
-        {   
-            enamyX2 = enamys.x;
-            enamyX = enamys2.x;
-            if(enamys.x>=1500)
+        {  
+           
+            if(enamyX <= -100)
             {
-                enamys.x = -100;
-            }
-            if(enamys.y >= 800)
-            {
-                enamys.x = -100;
-                
-            }
-            if(enamys.x <= -100)
-            {
-            enamyspeed = 8;
+            enamyspeed1 = 8;
             enamys.y = genarator.Next(400);
-            enamyY = enamys.y;
+            enamys.x = 0;
+            enamyX = enamys.x;
+            }
+            if(enamyX2 <= -100) 
+            {
+            enamyspeed2 = 8;
+            enamys2.y = genarator.Next(400);
+            enamys2.x= 0;
+            enamyX2 = enamys2.x;
+
             }
         }
     }
@@ -165,13 +174,17 @@ while (!Raylib.WindowShouldClose())
             player.y -= 70;
     }
 
-    if (Raylib.CheckCollisionRecs(enamys,player))
+    if (Raylib.CheckCollisionRecs(enamys2,player))
     {
     Raylib.DrawRectangleRec(player, Color.BROWN);
     Raylib.DrawRectangleRec(enamys, Color.YELLOW);
     Raylib.DrawRectangleRec(enamys2, Color.YELLOW);
     Raylib.ClearBackground(Color.WHITE);
-        
+    }
+    if (Raylib.CheckCollisionRecs(enamys,player))
+    {
+    Raylib.ClearBackground(Color.BLUE);
+
     }
     else
     {
